f5c86f4ea685a7e2b710cc1c8c62a72753326a07
aliyaskcodes <aliyashaikh0709@eng.rizvi.edu.in>
2026-04-02 08:36:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,14 +14,22 @@ print(f"NEET Students: {neet_students}")
 
 # Write your code here
 # TODO: Find and Print All the Students appearing for any Entrance Exam
+
+# TODO: Find and Print Students appearing for All Entrance Exams
+
+# TODO: Find and Print Students appearing for JEE but not for NEET
+
+# TODO: Find and Print Students appearing for CET and NEET but not for JEE
+print("List of Students:")
+
 all_students = cet | jee | neet
 print(f"All Students: {all_students}")
-# TODO: Find and Print Students appearing for All Entrance Exams
+
 all_exams = cet & jee & neet
 print(f"All Exams: {all_exams}")
-# TODO: Find and Print Students appearing for JEE but not for NEET
+
 jee_not_neet = jee - neet
 print(f"JEE but not for NEET: {jee_not_neet}")
-# TODO: Find and Print Students appearing for CET and NEET but not for JEE
+
 cet_and_neet_not_jee = (cet & neet) - jee
 print(f"CET and NEET but not for JEE: {cet_and_neet_not_jee}")
